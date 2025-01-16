b9b91307f8ab274ee994810eebaba8c95379cac9
Tony Tran <trantony0122@gmail.com>
2025-01-15 23:36:35 -0600

    fetched moves data

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState, useRef } from "react"
 import { getFullPokedexNumber, getPokedexNumber } from "../utils"
 import { TypeCard } from "./TypeCard"
+import { Modal } from "./Modal"
 
 const BASE_URL = "https://pokeapi.co/api/v2/pokemon/"
 
@@ -8,10 +9,57 @@ export function PokeCard(props) {
   const { selectedPokemon } = props
   const [data, setData] = useState(null)
   const [loading, setLoading] = useState(false)
+  const [skill, setSkill] = useState(null)
+  const [skillLoading, setSkillLoading] = useState(false)
 
   const abortControllerRef = useRef(null)
+  const skillAbortControllerRef = useRef(null)
 
   const { name, height, abilities, stats, types, moves, sprites } = data || {}
+
+  const imgList = Object.keys(sprites || {}).filter((sprite) => {
+    return sprites[sprite] !== null && !["versions", "other"].includes(sprite)
+  })
+
+  const skillList = moves?.filter((val) =>
+    val.version_group_details.some(
+      (detail) => detail.version_group.name === "firered-leafgreen"
+    )
+  )
+
+  const fetchMoveData = async (move, moveURL) => {
+    setSkillLoading(true)
+
+    const cache = JSON.parse(localStorage.getItem("pokedex-skills")) || {}
+    if (cache[move]) {
+      setSkill(cache[move])
+      setSkillLoading(false)
+      return
+    }
+
+    skillAbortControllerRef.current?.abort()
+    skillAbortControllerRef.current = new AbortController()
+    const signal = skillAbortControllerRef.current.signal
+
+    try {
+      const response = await fetch(moveURL, { signal })
+      const data = await response.json()
+      const skillDescriptionList = data?.flavor_text_entries.filter(
+        (val) => val.version_group.name === "firered-leafgreen"
+      )
+      const skillObj = {
+        name: move,
+        description: skillDescriptionList[0].flavor_text,
+      }
+      setSkill(skillObj)
+      cache[move] = skillObj
+      localStorage.setItem("pokedex-skills", JSON.stringify(cache))
+    } catch (e) {
+      console.log(e)
+    } finally {
+      setSkillLoading(false)
+    }
+  }
 
   useEffect(() => {
     const cache = JSON.parse(localStorage.getItem("pokedex")) || {}
@@ -33,7 +81,6 @@ export function PokeCard(props) {
         )
         const pokemonData = await response.json()
         setData(pokemonData)
-        console.log(pokemonData)
         cache[selectedPokemon] = pokemonData
         localStorage.setItem("pokedex", JSON.stringify(cache))
       } catch (e) {
@@ -56,6 +103,33 @@ export function PokeCard(props) {
 
   return (
     <div className='poke-card'>
+      {skillLoading && (
+        <Modal>
+          <p>Skill Loading...</p>
+        </Modal>
+      )}
+      {skill && (
+        <Modal
+          handleCloseModal={() => {
+            setSkill(null)
+          }}
+        >
+          <div>
+            <h6>Name</h6>
+            <h2>
+              {skill.name
+                .replaceAll("-", " ")
+                .split(" ")
+                .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+                .join(" ")}
+            </h2>
+          </div>
+          <div>
+            <h6>Description</h6>
+            <p>{skill.description}</p>
+          </div>
+        </Modal>
+      )}
       <div>
         <h4>#{getFullPokedexNumber(selectedPokemon)}</h4>
         <h2>{name}</h2>
@@ -64,6 +138,50 @@ export function PokeCard(props) {
       <div className='type-container'>
         {types.map((typeObj, index) => {
           return <TypeCard key={index} type={typeObj?.type.name} />
+        })}
+      </div>
+
+      <img
+        className='default-img'
+        src={"/pokemon/" + getFullPokedexNumber(selectedPokemon) + ".png"}
+        alt={`${name}-large-img`}
+      />
+      <div className='img-container'>
+        {imgList.map((spriteKey, index) => {
+          const imgUrl = sprites[spriteKey]
+          return <img key={index} src={imgUrl} />
+        })}
+      </div>
+
+      <h3>Stats</h3>
+
+      <div className='stats-card'>
+        {stats.map((statObj, index) => {
+          const { stat, base_stat } = statObj
+
+          return (
+            <div className='stat-item' key={index}>
+              <p>{stat?.name.replaceAll("-", " ")}</p>
+              <h4>{base_stat}</h4>
+            </div>
+          )
+        })}
+      </div>
+
+      <h3>Moves</h3>
+      <div className='pokemon-move-grid'>
+        {skillList.map((moveObj, index) => {
+          return (
+            <button
+              onClick={() =>
+                fetchMoveData(moveObj?.move?.name, moveObj?.move?.url)
+              }
+              key={index}
+              className='button-card pokemon-move'
+            >
+              <p>{moveObj?.move?.name.replace("-", " ")}</p>
+            </button>
+          )
         })}
       </div>
     </div>
